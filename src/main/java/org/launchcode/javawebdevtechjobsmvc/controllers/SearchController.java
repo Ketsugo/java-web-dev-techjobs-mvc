7e39d2112142cfc0d18fbc80e898500929456c2d
Tyler <ketsugo.h@gmail.com>
2021-03-17 02:02:06 -0500

    TODO 3/4 - Search Functionality added and results are displayed. Also moved job results table to fragments.html and put that fragment where necessary.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -23,6 +23,23 @@ public class SearchController {
         return "search";
     }
 
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+
+        ArrayList<Job> jobs;
+
+        if(searchType.equals("All") || searchTerm.equals("all") || searchTerm.equals("")){
+            jobs = JobData.findAll();
+        }
+        else{
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
+
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
 }
